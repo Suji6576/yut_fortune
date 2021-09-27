50fa6ea6c28284a681302e54706cd40c91ede0dd
안수지 <wwsa2@naver.com>
2021-09-27 13:22:14 +0900

    #4 시작화면 UI  - BaseActivity도 생성

--- app/src/main/java/com/neppplus/yut_fortune/SplashActivity.kt
+++ app/src/main/java/com/neppplus/yut_fortune/SplashActivity.kt
@@ -3,10 +3,10 @@ package com.neppplus.yut_fortune
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         setupEvents()
         setValues()
     }
