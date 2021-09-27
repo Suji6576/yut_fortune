1d107001163135775d8e1557f8108baf2f14ed31
안수지 <wwsa2@naver.com>
2021-09-27 17:02:24 +0900

    #1 숫자 랜덤생성기능 추가

--- app/src/main/java/com/neppplus/yut_fortune/MainActivity.kt
+++ app/src/main/java/com/neppplus/yut_fortune/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.neppplus.yut_fortune
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.yut_fortune.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+
+        binding.randomBtn.setOnClickListener {
+
+            binding.firstTxt.text = randomValues().toString()
+            binding.secondTxt.text = randomValues().toString()
+            binding.thirdTxt.text = randomValues().toString()
+
+            return@setOnClickListener
+        }
+
+    }
+
+    fun randomValues(): Int {
+
+        val randomNumber = (1..4).random()
+        return randomNumber
 
     }
 
