989af7443eeea4f02fee273c2b1c0cb8e49293aa
안수지 <wwsa2@naver.com>
2021-09-29 18:13:41 +0900

    #8 어댑터 작업중

--- app/src/main/java/com/neppplus/yut_fortune/ViewFortuneListActivity.kt
+++ app/src/main/java/com/neppplus/yut_fortune/ViewFortuneListActivity.kt
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.neppplus.yut_fortune.adapters.FortuneAdapter
 import com.neppplus.yut_fortune.databinding.ActivityViewFortuneListBinding
+import com.neppplus.yut_fortune.datas.FortuneData
 
 class ViewFortuneListActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewFortuneListBinding
+
+    val mMyFortuneList = ArrayList<FortuneData>()
+
+    lateinit var mFortuneAdapter : FortuneAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +33,11 @@ class ViewFortuneListActivity : BaseActivity() {
         titleTxt.text = "내 운세 목록"
 
         menuBtn.visibility = View.GONE
+
+        binding.roadRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
+        mFortuneAdapter = FortuneAdapter(mContext, mMyFortuneList)
+        binding.roadRecyclerView.adapter = mFortuneAdapter
 
     }
 }
--- app/src/main/java/com/neppplus/yut_fortune/adapters/FortuneAdapter.kt
+++ app/src/main/java/com/neppplus/yut_fortune/adapters/FortuneAdapter.kt
@@ -1,8 +1,54 @@
 package com.neppplus.yut_fortune.adapters
 
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.yut_fortune.R
 import com.neppplus.yut_fortune.datas.FortuneData
 
-class FortuneAdapter() : ArrayList<FortuneData>() {
+class FortuneAdapter(
+    val mContext: Context,
+    val mList: List<FortuneData>) : RecyclerView.Adapter<FortuneAdapter.ViewFortuneHolder>() {
+
+    class ViewFortuneHolder(view: View) :RecyclerView.ViewHolder(view){
+
+        val viewListDateTxt = view.findViewById<TextView>(R.id.viewListDateTxt)
+        val viewListCategoryTxt = view.findViewById<TextView>(R.id.viewListCategoryTxt)
+        val viewListWorryTxt = view.findViewById<TextView>(R.id.viewListWorryTxt)
+        val viewListFortuneTxt = view.findViewById<TextView>(R.id.viewListFortuneTxt)
+        val viewListItemLayout = view.findViewById<LinearLayout>(R.id.viewListItemLayout)
+
+        fun setRealData(data: FortuneData){
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewFortuneHolder {
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.view_fortune_list_item, parent, false)
+        return  ViewFortuneHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewFortuneHolder, position: Int) {
+
+        holder.setRealData(mList[position])
+
+        holder.viewListItemLayout.setOnClickListener {
+
+            Toast.makeText(mContext, "리스트 클릭", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount() = mList.size
 
 
 }
